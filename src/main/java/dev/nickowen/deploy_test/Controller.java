b00abcca44c5110c0e8af7f27cb8bcfe28c5f926
nicko1teck <nicko1teck@gmail.com>
2024-11-08 13:41:01 -0600

    changed requestMapping to getMapping

--- src/main/java/dev/nickowen/deploy_test/Controller.java
+++ src/main/java/dev/nickowen/deploy_test/Controller.java
@@ -1,12 +1,12 @@
 package dev.nickowen.deploy_test;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String welcome() {
 		return "You did it.";
 	}
