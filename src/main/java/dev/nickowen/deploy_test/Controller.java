4e53a3a0c17b0e6ca658b90f83230bd255b370d2
nicko1teck <nicko1teck@gmail.com>
2024-11-09 16:00:57 -0600

    trying a non-empty path in the controller to try and solve this 404 issue

--- src/main/java/dev/nickowen/deploy_test/Controller.java
+++ src/main/java/dev/nickowen/deploy_test/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 	
-	@GetMapping("/")
+	@GetMapping("/welcome")
 	public String welcome() {
 		return "You did it.";
 	}
