095a59852fdcff8b969126b46fb3679514d72b5a
nicko1teck <nicko1teck@gmail.com>
2024-11-10 14:14:54 -0600

    https://coderanch.com/t/747211/java/resolve-requested-resource-error-spring

--- src/main/java/dev/nickowen/deploy_test/DeployTestApplication.java
+++ src/main/java/dev/nickowen/deploy_test/DeployTestApplication.java
@@ -2,9 +2,16 @@ package dev.nickowen.deploy_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DeployTestApplication {
+public class DeployTestApplication extends SpringBootServletInitializer{
+	
+	@Override
+	 protected SpringApplicationBuilder configure( SpringApplicationBuilder builder) {
+	    return builder.sources(DeployTestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeployTestApplication.class, args);
