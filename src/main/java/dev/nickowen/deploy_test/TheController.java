b424af176360f002e9753a5424bf386aeba06e32
nicko1teck <nicko1teck@gmail.com>
2024-11-09 19:39:58 -0600

    renamed contoller class to then import Controller annotation, also added @requestbody

--- src/main/java/dev/nickowen/deploy_test/TheController.java
+++ src/main/java/dev/nickowen/deploy_test/TheController.java
@@ -1,10 +1,11 @@
 package dev.nickowen.deploy_test;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class Controller {
+
+@Controller
+public class TheController {
 	
 	@GetMapping("/welcome")
 	public String welcome() {
