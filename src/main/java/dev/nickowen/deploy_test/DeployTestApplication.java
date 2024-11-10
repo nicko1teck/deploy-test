cb24574e52d5ecd1e0a882716d7eb4a93a884f77
nicko1teck <nicko1teck@gmail.com>
2024-11-10 17:15:22 -0600

    trying adding an index.html and web-inf folder to hold it

--- src/main/java/dev/nickowen/deploy_test/DeployTestApplication.java
+++ src/main/java/dev/nickowen/deploy_test/DeployTestApplication.java
@@ -1,11 +1,14 @@
 package dev.nickowen.deploy_test;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+
 public class DeployTestApplication extends SpringBootServletInitializer{
 	
 	@Override
